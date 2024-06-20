a5897b0c26bd2b65d8efb3a48b20d5e2b728e7ae
ingong <insong.lee@webtoonscorp.com>
2024-06-20 11:35:40 +0900

    feat: day9

--- src/app/pages/chattings/chatting/chatting.component.ts
+++ src/app/pages/chattings/chatting/chatting.component.ts
@@ -1,12 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-chatting',
   standalone: true,
-  imports: [CommonModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './chatting.component.html',
   styleUrl: './chatting.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ChattingComponent {}
+export class ChattingComponent {
+  chattingForm = new FormGroup({
+    message: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(3)],
+    }),
+  });
+
+  get message() {
+    return this.chattingForm.controls.message;
+  }
+
+  message$ = this.message.valueChanges;
+
+  protected onSubmit(e: SubmitEvent) {}
+}
